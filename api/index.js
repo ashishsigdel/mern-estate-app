105e762e8d0132fbd22b82a7b936ebed8644b256
Ashish Sigdel <a.asis.sigdel01@gmail.com>
2023-11-21 12:28:03 +0545

    Connect to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to mongoDB.");
+}).catch((error) => {
+    console.log(error);
+});
 
 const app = express();
 
