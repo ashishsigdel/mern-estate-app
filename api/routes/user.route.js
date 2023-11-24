ab9078fa3987a6bad08f3cd6cb0ee3112769bc55
Ashish Sigdel <a.asis.sigdel01@gmail.com>
2023-11-24 14:45:12 +0545

    Add delete user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controllers.js";
+import {
+  deleteUser,
+  test,
+  updateUser,
+} from "../controllers/user.controllers.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from "../redux/user/userSlice";
 
 export default function Profile() {
@@ -68,7 +71,7 @@ export default function Profile() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if(data.success===false){
+      if (data.success === false) {
         dispatch(updateUserFailure(data.message));
         return;
       }
@@ -76,6 +79,22 @@ export default function Profile() {
       setUpdateSuccess(true);
     } catch (error) {
       dispatch(updateUserFailure(error.message));
+    }
+  };
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
     }
   };
   return (
@@ -129,16 +148,26 @@ export default function Profile() {
           className="p-3 border rounded-lg "
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-800 p-3 text-white uppercase font-semibold rounded-lg disabled:bg-slate-600 hover:bg-slate-900 transition duration-500 ">
+        <button
+          disabled={loading}
+          className="bg-slate-800 p-3 text-white uppercase font-semibold rounded-lg disabled:bg-slate-600 hover:bg-slate-900 transition duration-500 "
+        >
           {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between p-3">
-        <span className="text-red-600 cursor-pointer">Delete Account</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-600 cursor-pointer"
+        >
+          Delete Account
+        </span>
         <span className="text-red-600 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 text-center">{error ? error : ""}</p>
-      <p className="text-green-700 text-center">{updateSuccess ? "Update successfully" : ""}</p>
+      <p className="text-green-700 text-center">
+        {updateSuccess ? "Update successfully" : ""}
+      </p>
     </div>
   );
 }
