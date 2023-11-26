74f939964ae1ea0c0599cab97a6f6c6c05dec9ea
Ashish Sigdel <a.asis.sigdel01@gmail.com>
2023-11-26 15:04:07 +0545

    Create updata listing API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,11 +3,13 @@ import { verifyToken } from "../utils/verifyUser.js";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controllers.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
