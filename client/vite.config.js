dc0cda816ce908371db41b2c2a5e924b5c62a817
Ashish Sigdel <a.asis.sigdel01@gmail.com>
2024-07-06 15:05:19 +0545

    Ready for vercel deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
+      "/api": {
+        target: "http://localhost:3000/",
         secure: false,
+      },
     },
   },
-},
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure the output directory is set to 'dist'
+  },
 });
